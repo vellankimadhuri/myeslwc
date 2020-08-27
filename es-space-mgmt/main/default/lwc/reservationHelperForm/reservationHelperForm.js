8195735ab881973c4363f250216c8f6811a74ea2
Madhuri Vellanki <vellanki.madhuri@gmail.com>
2020-08-28 08:33:34 +1000

    reservationHelperForm corresponding components added.

---
+++ es-space-mgmt/main/default/lwc/reservationHelperForm/reservationHelperForm.js
@@ -0,0 +1,66 @@
+import { LightningElement, api } from 'lwc';
+import { FlowNavigationNextEvent } from 'lightning/flowSupport';
+
+export default class reservationHelperForm extends LightningElement {
+    //Flow input variables
+    @api customerid;
+    @api objecttype;
+    currentstate;
+    @api
+    get state() {
+        return this.currentstate;
+    }
+    set state(value) {
+        this.currentstate = value;
+    }
+
+    //Flow output variables
+    _endDays = 7;
+    _numberOfPeople = 20;
+
+    @api
+    get startDate() {
+        return this._startDate;
+    }
+
+    @api
+    get endDays() {
+        return this._endDays;
+    }
+
+    @api
+    get numberOfPeople() {
+        return this._numberOfPeople;
+    }
+
+    @api
+    get requestedMarket() {
+        return this._requestedMarket;
+    }
+
+    customerFields = [];
+
+    get detailsLoaded() {
+        return this.customerFields.length > 0 ? true : false;
+    }
+
+    handleDetails(event) {
+        this.customerFields = event.detail;
+    }
+
+    handleCustomerUpdate(event) {
+        if (this.currentstate !== event.detail) {
+            this.currentstate = event.detail;
+        }
+    }
+
+    handleDraftReservation(event) {
+        this._startDate = event.detail.startDate;
+        this._endDays = event.detail.endDays;
+        this._numberOfPeople = event.detail.numberOfPeople;
+        this._requestedMarket = event.detail.requestedMarket;
+
+        const nextNavigationEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(nextNavigationEvent);
+    }
+}
